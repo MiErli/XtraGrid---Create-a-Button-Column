4eaa12d187c7e17030952b20406210e163a7fcd6
Michael Erlinger <michael.erlinger@mbit.at>
2017-10-11 10:21:57 +0200

    Add project files.

---
+++ WindowsFormsApplication1/DataHelper_BindingList.cs
@@ -0,0 +1,114 @@
+﻿using DevExpress.XtraGrid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+
+namespace ProjectHelper
+{
+	  public class Record
+	{
+		public int Id { get; set; }
+		public int IdRandom { get; set; }
+		public  bool Check { get; set; }
+		public Image RandomImage { get; set; }
+		public string Text { get; set; }
+		public DateTime Date { get; set; }
+
+		public override string ToString()
+		{
+			return Text;
+		}
+	}
+
+	public class DataHelper 
+	{
+		public DataHelper()
+		{
+			recordBindingList = new BindingList<Record>();
+			AddDataToSource(20);
+		}
+
+		private GridControl _TargetControl;
+		private BindingList<Record> recordBindingList;
+
+		public GridControl TargetControl
+		{
+			get
+			{
+				return _TargetControl;
+			}
+			set
+			{
+				_TargetControl = value;
+				ResetDataSource();
+			}
+		}
+
+		public BindingList<Record> RecordBindingList
+		{
+			get
+			{
+				return recordBindingList;
+			}
+			set
+			{
+				recordBindingList = value;
+				ResetDataSource();
+			}
+		}
+
+		private void ResetDataSource()
+		{
+			if(_TargetControl == null) return;
+			_TargetControl.DataSource = recordBindingList;
+		}
+
+		public void AddDataToSource(object count)
+		{
+			AddDataToSource(Convert.ToInt32(count));
+		}
+
+		  public void AddDataToSource(int count)
+		{
+			Random r = new Random();
+			for (int i = 0; i < count; i++)
+				recordBindingList.Add(new Record()
+				{
+					Id = recordBindingList.Count,
+					Text = GetRandomString(r),
+					Date = DateTime.Now + new TimeSpan(i, i, i, i),
+					Check = r.Next(1,count) % 2 == 0,
+					RandomImage = GetRandomImage(50,50,r),
+					IdRandom = r.Next(0, count)
+				});
+		}
+
+		public Image GetRandomImage(int w, int h, Random r)
+		{
+		   
+			Color randomColor = GetRandomColor(r);
+			Image img = new Bitmap(w, h);
+			using (Graphics g = Graphics.FromImage(img))
+				g.FillRectangle(new SolidBrush(randomColor), new Rectangle(0, 0, img.Width, img.Height));
+			return img;
+		}
+
+		public Color GetRandomColor(Random r)
+		{
+			Thread.Sleep(3);
+			return Color.FromArgb(r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+		}
+
+		const string letters = "sqwesrty uiopa sdfsghj klszx cvsbsnm abcd efg hig klmnop q rstu vwxyz kojnianw asdqwae d";
+		public string GetRandomString(Random r)
+		{
+			int start = r.Next(letters.Length - 5);
+			int length = r.Next(letters.Length - start - 1);
+			return letters.Substring(start, length);
+		}
+	}
+}
---
+++ WindowsFormsApplication1/MVVMHelper.cs
@@ -0,0 +1,63 @@
+﻿using DevExpress.XtraEditors.Controls;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Columns;
+using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace WindowsFormsApplication1
+{
+    public class MVVMHelper
+    {
+        public static void AssignDataSourceToGrid(GridControl grid, object dataSource)
+        {
+            grid.DataSource = dataSource;
+            CreateButtonColumn(grid);
+        }
+
+        static void CreateButtonColumn(GridControl grid)
+        {
+            GridView view = grid.MainView as GridView;
+            GridColumn colButton = view.Columns.AddVisible("colButton");
+            colButton.UnboundType = DevExpress.Data.UnboundColumnType.Object;
+            colButton.ColumnEdit = CreateEdit(grid);
+        }
+
+        static RepositoryItemButtonEdit CreateEdit(GridControl grid)
+        {
+            RepositoryItemButtonEdit edit = new RepositoryItemButtonEdit{
+                            TextEditStyle = TextEditStyles.HideTextEditor,};
+
+            edit.Buttons.Clear();
+            var x = new EditorButton(ButtonPredefines.Glyph, "Edit", -1, true, true, false, DevExpress.XtraEditors.ImageLocation.MiddleLeft, GetButtonImage());
+            x.Click += X_Click;
+            edit.Buttons.AddRange(new EditorButton[] { x });
+                           
+
+            grid.RepositoryItems.Add(edit);
+            return edit;
+        }
+
+        private static void X_Click(object sender, EventArgs e)
+        {
+            DevExpress.XtraEditors.XtraMessageBox.Show("hallo", "Hallo DU welt.");
+        }
+
+        static Image GetButtonImage()
+        {
+            int w = 16;
+            int h = 16;
+            Brush b = Brushes.Orange;
+            Image img = new Bitmap(w, h);
+            using (Graphics g = Graphics.FromImage(img))
+            {
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                g.FillEllipse(b, new Rectangle(0, 0, w - 1, h - 1));
+            }
+            return img;
+        }
+    }
+}
